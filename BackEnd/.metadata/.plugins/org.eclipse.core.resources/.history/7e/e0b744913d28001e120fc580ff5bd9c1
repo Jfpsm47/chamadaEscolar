8e57e967bdc64349d6cbe6470dd529026dc18094
João Flávio Pompeo <jfpompeo47@gmail.com>
2023-07-21 23:58:54 -0400

    entidades Aluno e Turma adicionadas e relacionadas

---
+++ BackEnd/.metadata/.plugins/org.eclipse.core.resources/.history/7e/e0b744913d28001e120fc580ff5bd9c1
@@ -0,0 +1,13 @@
+package com.example.chamada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiChamadaEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiChamadaEscolarApplication.class, args);
+	}
+
+}
---
+++ BackEnd/ApiChamadaEscolar/src/main/java/com/example/chamada/model/turma/Turma.java
@@ -0,0 +1,70 @@
+package com.example.chamada.model.turma;
+
+import java.util.List;
+
+import com.example.chamada.model.aluno.Aluno;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Turma {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private String turno;
+	
+	@OneToMany(mappedBy = "turma")
+	@JsonIgnore
+	private List<Aluno> alunos;
+
+	public Turma() {
+		super();
+	}
+
+	public Turma(String nome, String turno) {
+		super();
+		this.nome = nome;
+		this.turno = turno;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTurno() {
+		return turno;
+	}
+
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
+
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+	
+	
+}
