c41ac6ba32eaaa9c8187c1499a6ab5cd4d0882e6
João Flávio Pompeo <jfpompeo47@gmail.com>
2023-07-21 22:24:43 -0400

    commit inicial

---
+++ BackEnd/ApiChamadaEscolar/src/test/java/com/example/chamada/ApiChamadaEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chamada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChamadaEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
